41504291b12918435bd735c2133d88cd6edf4d38
IvoCobos <ivancho3110@hotmail.com>
2022-05-27 17:57:27 -0500

    ideas

--- laberinto/laberinto.js
+++ laberinto/laberinto.js
@@ -1,6 +1,16 @@
-    const numeros = [0,0,1,0,0];
-    numeros.forEach(function(numeros) {
-        if(numeros == 1){
-            console.log(numeros)
-        }
-    });
+	var maze = new Array();
+
+	for (var rows = 0; rows < 5; rows++) {
+		maze[rows] = new Array();
+	}
+	maze[0][0] = new Array(1, 1, 1, 1, 1);
+    maze[1][0] = new Array(1, 0, 1, 1, 1);
+	maze[2][0] = new Array(1, 1, 0, 1, 1);
+    maze[3][0] = new Array(1, 1, 1, 0, 1);
+    maze[3][0] = new Array(1, 1, 1, 0, 0);
+    
+    console.log(maze)
+
+    for(let i = 0; maze.length == 1; i++){
+        console.log(maze[i]);
+    }
