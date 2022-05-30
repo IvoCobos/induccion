b0a55f70624ff339bcc8760ca60f0353a8e30cb7
IvoCobos <ivancho3110@hotmail.com>
2022-05-30 12:58:39 -0500

    Visual elements

--- laberinto/laberinto.js
+++ laberinto/laberinto.js
@@ -1,16 +1,18 @@
-	var maze = new Array();
+var maze = new Array();
 
-	for (var rows = 0; rows < 5; rows++) {
-		maze[rows] = new Array();
-	}
-	maze[0][0] = new Array(1, 1, 1, 1, 1);
-    maze[1][0] = new Array(1, 0, 1, 1, 1);
-	maze[2][0] = new Array(1, 1, 0, 1, 1);
-    maze[3][0] = new Array(1, 1, 1, 0, 1);
-    maze[3][0] = new Array(1, 1, 1, 0, 0);
-    
-    console.log(maze)
+for (var rows = 0; rows < 5; rows++) {
+    maze[rows] = new Array();
+}
+maze[0] = new Array(1, 1, 1, 1, 1, 1);
+maze[1] = new Array(1, 0, 1, 1, 0, 1);
+maze[2] = new Array(1, 1, 1, 1, 0, 1);
+maze[3] = new Array(0, 0, 1, 0, 1, 1);
+maze[3] = new Array(0, 0, 1, 0, 1, 0);
+maze[4] = new Array(0, 0, 1, 1, 1, 1);
 
-    for(let i = 0; maze.length == 1; i++){
-        console.log(maze[i]);
-    }
+
+
+for(let i=0; 1 == maze.length; i++){
+        console.log(maze)
+}
+
