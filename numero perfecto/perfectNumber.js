81b5fbf378355e476351f3994505eb72e985d7be
IvoCobos <ivancho3110@hotmail.com>
2022-06-01 15:51:51 -0500

    text perfect number

--- numero perfecto/perfectNumber.js
+++ numero perfecto/perfectNumber.js
@@ -4,7 +4,7 @@ for (let i = 2; n <= 10000; i++) {
     perfect = (2 ** (i - 1)) * (2 ** i - 1);
 
     if (perfect <= 10000) {
-        console.log(perfect);
+        console.log("numero perfecto:", perfect);
     } else if (perfect > 10000) {
         break
     }
