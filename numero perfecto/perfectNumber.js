1ea1ad608e51da7f95d8a60620fa61654f77b7f1
IvoCobos <ivancho3110@hotmail.com>
2022-06-01 14:28:22 -0500

    perfect number 1 to 10000

---
+++ numero perfecto/perfectNumber.js
@@ -0,0 +1,11 @@
+var perfect = [];
+const n = 10000;
+for (let i = 2; n <= 10000; i++) {
+    perfect = (2 ** (i - 1)) * (2 ** i - 1);
+
+    if (perfect <= 10000) {
+        console.log(perfect);
+    } else if (perfect > 10000) {
+        break
+    }
+}
