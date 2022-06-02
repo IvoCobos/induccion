1efd981e58486c8d1745d29a4b45f1554e64008d
IvoCobos <ivancho3110@hotmail.com>
2022-06-02 15:53:37 -0500

    mazeGame

--- laberinto/laberinto.js
+++ laberinto/laberinto.js
@@ -1,8 +1,5 @@
 var maze = new Array();
 
-for (var rows = 0; rows < 5; rows++) {
-    maze[rows] = new Array();
-}
 maze[0] = new Array(1, 1, 1, 1, 1, 1);
 maze[1] = new Array(1, 0, 1, 1, 0, 1);
 maze[2] = new Array(1, 1, 1, 1, 0, 1);
