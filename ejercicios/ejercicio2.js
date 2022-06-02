eec48791c2b3766473696c44bc5647e8c56a461c
IvoCobos <ivancho3110@hotmail.com>
2022-06-02 13:58:18 -0500

    new maze

--- ejercicios/ejercicio2.js
+++ ejercicios/ejercicio2.js
@@ -3,5 +3,5 @@ process.stdout.write("Ingrese una frase: ");//muestro en consola un mensaje
 
 process.stdin.on('data', function (limite) {//ingreso datos por consola
     limite = limite.toString();//transformo el dato ingresado por consola en numero
-    console.log(limite);
+    // console.log(limite);
 });
