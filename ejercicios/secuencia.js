0ec011afc077c2731789213728106be612526550
IvoCobos <ivancho3110@hotmail.com>
2022-06-08 16:26:50 -0500

    changes secuancial

--- ejercicios/secuencia.js
+++ ejercicios/secuencia.js
@@ -1,13 +1,13 @@
 function generateSecuencia(number){
     var secuencia = [];
-    for (let index = 0; index >= 0 ; index +=2 ) {
+    for (let i = 0; i >= 0 ; i+=2 ) {
         var canBreak = false;
-        for (let index1 = 1; index1 <= 4; index1++) {
-            secuencia = (index + index1);
-            console.log(secuencia);
+        for (let j = 1; j <= 4; j++) {
+            secuencia = (i + j);
         }
         if(canBreak === true){
             break
         }
+        
     }
 }
